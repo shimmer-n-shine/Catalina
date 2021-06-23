b7f7d51fd990381ab2e1af6980aeb0f0358c8adb
Artin Noyan <artinnoyan@gmail.com>
2021-06-24 01:52:24 +0400

    multi server support

--- Catalina/Discord/Events.cs
+++ Catalina/Discord/Events.cs
@@ -5,7 +5,6 @@ using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Catalina.Discord
@@ -13,8 +12,6 @@ namespace Catalina.Discord
     class Events
     {
         static ConfigValues ConfigValues => ConfigValues.configValues;
-        static List<Response> responses = ConfigValues.Responses;
-        static List<Reaction> reactions = ConfigValues.Reactions;
 
         internal static Task Discord_GuildMemberRemoved(DiscordClient sender, GuildMemberRemoveEventArgs e)
         {
@@ -23,32 +20,36 @@ namespace Catalina.Discord
 
         internal static async Task Discord_MessageCreated(DiscordClient sender, MessageCreateEventArgs e)
         {
-            if (responses.Select(response => response.trigger).Contains(e.Message.Content))
-            {
-                var response = responses.Find(r => r.trigger == e.Message.Content);
-                if (response.disallowedChannels == null || !response.disallowedChannels.Contains(e.Channel)) {
-                    await e.Message.RespondAsync(response.response);
-                }
-            }
+            throw new NotImplementedException();
         }
 
         internal static async Task Discord_ReactionAdded(DiscordClient sender, MessageReactionAddEventArgs e)
         {
-            if (reactions.Select(reaction => reaction.messageID).Contains(e.Message.Id) && reactions.Select(reaction => reaction.emoji).Contains(e.Emoji) && !e.User.IsBot) {
-                var reaction = reactions.Find(r => r.messageID == e.Message.Id && r.emoji == e.Emoji);
-                var member = await e.Guild.GetMemberAsync(e.User.Id);
-                await member.GrantRoleAsync(reaction.role, "Assigned role upon reaction");
+            
+            if (ConfigValues.Reactions.ContainsKey(e.Guild.Id))
+            {
+                var reactions = ConfigValues.Reactions[e.Guild.Id];
+                if (reactions.Select(reaction => reaction.messageID).Contains(e.Message.Id) && reactions.Select(reaction => reaction.emojiID).Contains(e.Emoji.Id) && !e.User.IsBot)
+                {
+                    var reaction = reactions.Find(r => r.messageID == e.Message.Id && r.emojiID == e.Emoji.Id);
+                    var member = await e.Guild.GetMemberAsync(e.User.Id);
+                    await member.GrantRoleAsync(e.Guild.GetRole(reaction.roleID), "Assigned role upon reaction");
+                }
             }
         }
 
         internal static async Task Discord_ReactionRemoved(DiscordClient sender, MessageReactionRemoveEventArgs e)
         {
-            if (reactions.Select(reaction => reaction.messageID).Contains(e.Message.Id) && reactions.Select(reaction => reaction.emoji).Contains(e.Emoji) && !e.User.IsBot)
+            if (ConfigValues.Reactions.ContainsKey(e.Guild.Id))
             {
-                var reaction = reactions.Find(r => r.messageID == e.Message.Id && r.emoji == e.Emoji);
-                var member = await e.Guild.GetMemberAsync(e.User.Id);
-                await member.RevokeRoleAsync(reaction.role, "Revoked role upon reaction");
+                var reactions = ConfigValues.Reactions[e.Guild.Id];
+                if (reactions.Select(reaction => reaction.messageID).Contains(e.Message.Id) && reactions.Select(reaction => reaction.emojiID).Contains(e.Emoji.Id) && !e.User.IsBot)
+                {
+                    var reaction = reactions.Find(r => r.messageID == e.Message.Id && r.emojiID == e.Emoji.Id);
+                    var member = await e.Guild.GetMemberAsync(e.User.Id);
+                    await member.RevokeRoleAsync(e.Guild.GetRole(reaction.roleID), "Revoked role upon reaction");
 
+                }
             }
         }
 
@@ -62,14 +63,18 @@ namespace Catalina.Discord
 
         internal static Task Discord_MessageDeleted(DiscordClient sender, MessageDeleteEventArgs e)
         {
-            if (reactions.Select(reaction => reaction.messageID).Contains(e.Message.Id))
+            if (ConfigValues.Reactions.ContainsKey(e.Guild.Id))
             {
-                var reactions = Events.reactions.FindAll(r => r.messageID == e.Message.Id);
-                reactions.ForEach(reaction =>
+                var reactions = ConfigValues.Reactions[e.Guild.Id];
+                if (reactions.Select(reaction => reaction.messageID).Contains(e.Message.Id))
                 {
-                    ConfigValues.Reactions.Remove(reaction);
-                });
-                ConfigValues.SaveConfig();
+                    var matches = reactions.FindAll(r => r.messageID == e.Message.Id);
+                    matches.ForEach(match =>
+                    {
+                        ConfigValues.Reactions[e.Guild.Id].Remove(match);
+                    });
+                    ConfigValues.SaveConfig();
+                }
             }
             Log.Information("Removed reactions from deleted message!");
             return Task.CompletedTask;
