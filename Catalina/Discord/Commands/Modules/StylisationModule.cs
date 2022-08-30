27919d3105eccb3ffdf4b9ca0a12fbc80ef3eaf3
Cataclyst <artinnoyan@gmail.com>
2022-08-30 18:48:30 +0400

    commands to rename reameable roles

--- Catalina/Discord/Commands/Modules/StylisationModule.cs
+++ Catalina/Discord/Commands/Modules/StylisationModule.cs
@@ -65,10 +65,62 @@ namespace Catalina.Discord.Commands.Modules
                 return;
 
             }
-            
-            
-
         }
+
+        [SlashCommand("name", "Change role name")]
+        public async Task ConfigureRole([Autocomplete(typeof(RoleStylisation))] string roleID, string roleName)
+        {
+            if (!ulong.TryParse(roleID, out ulong id))
+            {
+                await Context.Interaction.RespondAsync(embed: new Utils.ErrorMessage { User = Context.User, Exception = new ArgumentException() }, ephemeral: true);
+                return;
+            }
+
+            using var database = new DatabaseContextFactory().CreateDbContext();
+            var guildProperties = database.GuildProperties.Include(g => g.Roles).AsNoTracking().FirstOrDefault(g => g.ID == Context.Guild.Id);
+            Database.Models.Role DBrole;
+
+            DBrole = guildProperties.Roles.FirstOrDefault(r => r.ID == id);
+
+            if (DBrole == null)
+            {
+                DBrole = new Database.Models.Role { ID = id };
+                guildProperties.Roles.Add(DBrole);
+                database.SaveChanges();
+            }
+
+            try
+            {
+                var role = Context.Guild.GetRole(ulong.Parse(roleID));
+
+                var preliminaryGuildRoleResults = database.GuildProperties.Include(g => g.Roles).AsNoTracking().SelectMany(g => g.Roles).Where(r => r.IsColourable).Select(r => r.ID).ToList();
+                var preliminaryUserRoleResults = (Context.User as IGuildUser).RoleIds;
+
+                var results = preliminaryGuildRoleResults.Intersect(preliminaryUserRoleResults);
+
+
+                if (results.Contains(id))
+                {
+                    await role.ModifyAsync(r => r.Name = roleName);
+                    await RespondAsync(embed: new Utils.AcknowledgementMessage { User = Context.User, Body = $"Changed role name to {roleName}.", Color = CatalinaColours.Green });
+                }
+                else
+                {
+                    await Context.Interaction.RespondAsync(embed: new Utils.ErrorMessage { User = Context.User, Exception = new UnauthorizedAccessException() }, ephemeral: true);
+
+                }
+
+
+            }
+            catch
+            {
+                await Context.Interaction.RespondAsync(embed: new Utils.ErrorMessage { User = Context.User, Exception = new ArgumentException() }, ephemeral: true);
+                return;
+
+            }
+        }
+
+
 
         //[SlashCommand("name", "Change role name")]
         //public async Task ListRoles()
