00b89b98fbe958c889b630ccb62be728a22810ae
Cataclyst <artinnoyan@gmail.com>
2022-08-30 18:46:52 +0400

    add guild property relating to starboard

--- Catalina/Database/Models/GuildProperty.cs
+++ Catalina/Database/Models/GuildProperty.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace Catalina.Database.Models
+namespace Catalina.Database.Models;
+public class GuildProperty
 {
-    public class GuildProperty
-    {
-        [Key]
-        public ulong ID { get; set; }
-        public List<Role> Roles { get; set; } = new List<Role>();
-    }
+    [Key]
+    public ulong ID { get; set; }
+    public List<Role> Roles { get; set; } = new List<Role>();
+
+    public ulong? StarBoardChannel { get; set; } = null;
+
+    public Emoji? StarboardEmoji { get; set; } = null;
+
+    public int StarboardThreshhold { get; set; } = 5;
+
 }
