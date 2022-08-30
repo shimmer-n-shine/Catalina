21c2e13c65e20534632ea9d3c26029d5e920d4cd
Cataclyst <artinnoyan@gmail.com>
2022-08-30 18:47:39 +0400

    add dataset of emojis

--- Catalina/Database/DatabaseContext.cs
+++ Catalina/Database/DatabaseContext.cs
@@ -6,9 +6,10 @@ namespace Catalina.Database
     public class DatabaseContext : DbContext
     {
         public DatabaseContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
-
         public DbSet<GuildProperty> GuildProperties { get; set; }
         public DbSet<Response> Responses { get; set; }
         public DbSet<Reaction> Reactions { get; set; }
+
+        public DbSet<Emoji> Emojis { get; set; }
     }
 }
