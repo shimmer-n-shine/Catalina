017a68cff1cdb7578a9e8354ea457418ec1df8f3
CataclystP <artinnoyan@gmail.com>
2023-05-22 23:27:29 +0400

    introduced timezone settings model

---
+++ Catalina/Database/Models/TimezoneSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Catalina.Database.Models
+{
+    public class TimezoneSettings
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public ulong ID { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
