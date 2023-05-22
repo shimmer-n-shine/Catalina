d74c57b3508102d6f4c088ed4238b6e219acaf8e
CataclystP <artinnoyan@gmail.com>
2023-05-22 23:27:42 +0400

    database models updated, relationships clarified

--- Catalina/Database/DatabaseContext.cs
+++ Catalina/Database/DatabaseContext.cs
@@ -1,6 +1,6 @@
 ﻿using Catalina.Database.Models;
 using Microsoft.EntityFrameworkCore;
-using static Catalina.Database.Models.Starboard;
+using static Catalina.Database.Models.StarboardSettings;
 
 namespace Catalina.Database
 {
@@ -10,25 +10,28 @@ namespace Catalina.Database
         {
             this.ChangeTracker.LazyLoadingEnabled = true;
         }
-        public DbSet<GuildProperty> GuildProperties { get; set; }
+        public DbSet<Guild> GuildProperties { get; set; }
         public DbSet<Response> Responses { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Message> StarboardMessages { get; set; }
-        public DbSet<Starboard> Starboards { get; set; }
+        public DbSet<StarboardSettings> Starboards { get; set; }
         public DbSet<Emoji> Emojis { get; set; }
         public DbSet<Vote> StarboardVotes { get; set; }
+        public DbSet<TimezoneSettings> Timezones { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<GuildProperty>().HasMany(g => g.Responses);
-            modelBuilder.Entity<GuildProperty>().HasMany(g => g.Roles);
+            modelBuilder.Entity<Guild>().HasMany(g => g.Responses);
+            modelBuilder.Entity<Guild>().HasMany(g => g.Roles);
+            modelBuilder.Entity<Guild>().HasOne(g => g.Timezones);
 
-            modelBuilder.Entity<GuildProperty>().HasOne(g => g.Starboard);
+            modelBuilder.Entity<Guild>().HasOne(g => g.Starboard);
 
-            modelBuilder.Entity<Starboard>().HasMany(s => s.Messages);
-            modelBuilder.Entity<Starboard>().HasOne(s => s.Emoji);
+            modelBuilder.Entity<StarboardSettings>().HasMany(s => s.Messages);
+            modelBuilder.Entity<StarboardSettings>().HasOne(s => s.Emoji);
 
             modelBuilder.Entity<Message>().HasMany(m => m.Votes);
+
         }
     }
 }
