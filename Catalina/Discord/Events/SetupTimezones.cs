36d744ec0b06de2d7e59c41c642540077d5f36a0
CataclystP <artinnoyan@gmail.com>
2023-05-23 16:04:03 +0400

    added setup timezone event, auto-invoking

---
+++ Catalina/Discord/Events/SetupTimezones.cs
@@ -0,0 +1,16 @@
+﻿using Catalina.Core;
+using Catalina.Database;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace Catalina.Discord.Events;
+public static partial class Events 
+{
+    [Invoke(alignTo: AlignTo.OneMinute)]
+    internal static async Task SetupTimezones()
+    {
+        using var database = Services.GetRequiredService<DatabaseContext>();
+
+        await Timezones.GetRoles(Services, Discord.DiscordClient);
+    }
+}
