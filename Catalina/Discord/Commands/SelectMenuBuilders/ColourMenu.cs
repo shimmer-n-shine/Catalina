957f46253eec5d0684b996b9d6bda67b3a231e24
CataclystP <artinnoyan@gmail.com>
2023-05-25 00:25:53 +0400

    ISelectMenu and ColourMenu + for the new say command

---
+++ Catalina/Discord/Commands/SelectMenuBuilders/ColourMenu.cs
@@ -0,0 +1,40 @@
+﻿using Catalina.Common;
+using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalina.Discord.Commands.SelectMenuBuilders;
+public class ColourMenu : ISelectMenu
+{
+    public string ID { get; set; } = "colour_menu";
+    public string Placeholder { get; set; } = "colour menu";
+    public List<SelectMenuOptionBuilder> Options
+    {
+        get
+        {
+            var options = new List<SelectMenuOptionBuilder>();
+            var colours = CatalinaColours.ToDictionary();
+            var unicode = EmojiToolkit.Emoji.Get(":art:").Raw;
+            var emote = Emoji.Parse(unicode);
+            foreach (var colour in colours)
+            {
+                
+                options.Add(new SelectMenuOptionBuilder()
+                {
+                    Label = colour.Key,
+                    Value = colour.Key,
+                    Emote = emote
+                });
+            }
+            return options;
+        }
+    }
+
+    public SelectMenuBuilder ToSelectMenuBuilder()
+    {
+        return new SelectMenuBuilder(customId: ID, placeholder: Placeholder, options: Options);
+    }
+}
---
+++ Catalina/Discord/Commands/SelectMenuBuilders/ISelectMenu.cs
@@ -0,0 +1,16 @@
+﻿using Discord;
+using System.Collections.Generic;
+
+namespace Catalina.Discord.Commands.SelectMenuBuilders;
+public interface ISelectMenu
+{
+    public static string ID { get; set; }
+    public static string Placeholder { get; set; }
+
+    public static List<SelectMenuOptionBuilder> Options { get; set; }
+
+    public static SelectMenuBuilder ToSelectMenuBuilder() 
+    {
+        return new SelectMenuBuilder(customId: ID, placeholder: Placeholder, options: Options);
+    }
+}
