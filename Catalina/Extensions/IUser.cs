1595bded2f57808ffc91f96aaea33838c5f36461
CataclystP <artinnoyan@gmail.com>
2023-05-25 00:28:06 +0400

    QOL:  IUser.FullName() + allows a formatted username on any IUser, returning their username + discriminant if present

---
+++ Catalina/Extensions/IUser.cs
@@ -0,0 +1,12 @@
+﻿using Discord;
+
+namespace Catalina.Extensions;
+public static class IUserExtensions
+{
+    public static string FullName(this IUser user)
+    {
+        var userName = user.Username;
+        if (user.DiscriminatorValue != 0) userName += '#' + user.Discriminator;
+        return userName;
+    }
+}
